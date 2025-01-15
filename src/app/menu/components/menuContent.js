92e22721b7e1ec08792e0fb7c8a0ecf4f8426d83
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2025-01-15 13:20:24 -0500

    removed duplicate content for each menu component and moved into a reusable component

---
+++ src/app/menu/components/menuContent.js
@@ -0,0 +1,54 @@
+"use client";
+
+
+export default function MenuContent({filteredMenu, isDairyFree, isGlutenFree, setIsDairyFree, setIsGlutenFree}) {
+    return (
+      <section className="w-full">
+        <div className="lg:w-3/4 m-auto">
+          <div className="mb-6 flex flex-col gap-4">
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                className="mr-2"
+                checked={isGlutenFree}
+                onChange={() => setIsGlutenFree(!isGlutenFree)}
+              />
+              Show Gluten-Free Options Only
+            </label>
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                className="mr-2"
+                checked={isDairyFree}
+                onChange={() => setIsDairyFree(!isDairyFree)}
+              />
+              Show Dairy-Free Options Only
+            </label>
+          </div>
+          {Object.entries(filteredMenu).map(([category, items]) => (
+            <div key={category} className="mb-8">
+              <h3 className="text-3xl pt-4 lg:text-4xl font-semibold border-b-2 pb-2 mb-4">
+                {category}
+              </h3>
+              {items.map((item) => (
+                <div key={item.title} className="mb-6">
+                  <div className="flex justify-between items-center">
+                    <h3 className="text-xl font-semibold">{item.title}</h3>
+                    <p className="text-lg font-bold">{item.price}</p>
+                  </div>
+                  <p className="text-md text-gray-700 mt-2 lg:w-3/4">
+                    {item.description}
+                  </p>
+                  {item.additional && (
+                    <p className="text-md text-gray-500 italic mt-1">
+                      {item.additional}
+                    </p>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+}
