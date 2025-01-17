056f80774bc64a1e27a09a5711588522946aba81
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2025-01-17 11:01:05 -0500

    basic user auth added to visit the site

--- middleware.js
+++ middleware.js
@@ -1,10 +1,9 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export function middleware(req) {
-    const req = NextRequest;
+const req = NextRequest;
   const auth = req.headers.get('authorization');
-  const username = 'yourUsername';  // Change this to your desired username
-  const password = 'yourPassword';  // Change this to your desired password
+  const username = 'ducky'; 
   const encoded = Buffer.from(`${username}:${password}`).toString('base64');
 
   if (auth !== `Basic ${encoded}`) {
