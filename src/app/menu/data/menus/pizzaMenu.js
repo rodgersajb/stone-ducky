31721e01545cfd6d7ed8595b7601ce9186d4d7e9
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2025-01-22 12:32:24 -0500

    changed pizza data glutenFree boolean to true and added glutenFree Image

--- src/app/menu/data/menus/pizzaMenu.js
+++ src/app/menu/data/menus/pizzaMenu.js
@@ -1,3 +1,5 @@
+const glutenFree = "/images/gluten-free.png";
+
 const pizzaMenu = {
   Pizzas: [
     { title: "** Gluten-Free crusts are available for small pizzas only" },
@@ -6,64 +8,72 @@ const pizzaMenu = {
       price: 20,
       description:
         "San Marzano tomato sauce, fior di latte mozzarella, basil, drizzle of EVOO.",
-      glutenFree: false,
+      glutenFree: true,
       dairyFree: false,
+      image: glutenFree,
     },
     {
       title: "Pepperoni",
       price: 22,
       description:
         "San Marzano tomato sauce, Stone Ducky's 3-cheese blend, pepperoni, oregano.",
-      glutenFree: false,
+      glutenFree: true,
       dairyFree: false,
+      image: glutenFree,
     },
     {
       title: "Stone Ducky Supreme",
       price: 25,
       description:
         "San Marzano tomato sauce, Stone Ducky's 3-cheese blend, pepperoni, Newlander's Italian sausage, green peppers, mushrooms, red onion, black olives.",
-      glutenFree: false,
+      glutenFree: true,
       dairyFree: false,
+      image: glutenFree,
     },
     {
       title: "Meat Lovers",
       price: 27,
       description:
         "San Marzano tomato sauce, Stone Ducky's 3-cheese blend, pepperoni, Genoa salami, ham, Newlander Meat's Italian sausage.",
-      glutenFree: false,
+      glutenFree: true,
       dairyFree: false,
+      image: glutenFree,
     },
     {
       title: "BBQ Chicken",
       price: 22,
       description:
         "Bakeapple BBQ sauce, Stone Ducky's 3-cheese blend, grilled chicken, red onions.",
-      glutenFree: false,
+      glutenFree: true,
       dairyFree: false,
+      image: glutenFree,
     },
     {
       title: "Spicy Duck Diavolo",
       price: 23,
       description:
         "Firey garlic tomato sauce, Stone Ducky's 3-cheese blend, duck confit, roasted red peppers, red onions, jalapeños.",
-      glutenFree: false,
+      glutenFree: true,
       dairyFree: false,
+      image: glutenFree,
     },
     {
       title: "Vegetarian",
       price: 21,
       description:
         "San Marzano tomato sauce, Stone Ducky's 3-cheese blend, Windy Heights Farm's mushrooms, green pepper, red onion, black olives, tomato.",
-      glutenFree: false,
+      glutenFree: true,
       dairyFree: false,
+      image: glutenFree,
     },
     {
       title: "Chicken Alfredo",
       price: 22,
       description:
         "Roasted garlic cream, Stone Ducky's 3-cheese blend, Windy Heights Farm's mushroom, chicken, spinach.",
-      glutenFree: false,
+      glutenFree: true,
       dairyFree: false,
+      image: glutenFree,
     },
   ],
 };
