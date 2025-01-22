b885ec5ed856f5a1974ea7c84b8bb544ee899d43
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2025-01-22 12:26:38 -0500

    adjusted relative image positiong for sleepy duck in visit us section

--- src/app/components/visit.js
+++ src/app/components/visit.js
@@ -19,9 +19,9 @@ export default function Visit() {
           className="object-contain lg:object-cover"
         />
       </FadeInImageContainer>
-      <div className="min-h-[625px] lg:min-h-[500px] w-full text-coolGray flex flex-col items-center justify-evenly gap-4">
+      <div className="min-h-[625px] lg:min-h-[500px] w-full text-coolGray flex flex-col items-center justify-evenly  lg:justify-center gap-4 ">
         <div className="z-10 relative">
-          <h2 className="text-2xl lg:text-4xl relative pb-2">Visit Us</h2>
+          <h2 className="text-2xl lg:text-4xl relative  ">Visit Us</h2>
         </div>
 
         <div className="lg:text-xl text-center font-semibold">
@@ -30,28 +30,32 @@ export default function Visit() {
           <p>(709) 334-3825</p>
         </div>
 
-        <div className="flex gap-2 lg:text-xl font-semibold">
-          <p>Tuesday/Thursday</p>
-          <p>11:00 am - 8:00 pm</p>
+        <div className="flex flex-col lg:flex-row  justify-center lg:justify-around lg:w-[85%] items-center gap-2 lg:text-xl ">
+          <div className="text-center">
+            <p className="font-semibold">Tuesday/Thursday</p>
+            <p>11:00 am - 8:00 pm</p>
+          </div>
+          <div className="text-center">
+            <p className="font-semibold">Friday/Sunday</p>
+            <p>11:00 am - 10:00 pm</p>
+          </div>
         </div>
-        <div className="z-10 flex gap-2 lg:text-xl font-semibold">
-          <p>Friday/Sunday</p>
-          <p>11:00 am - 10:00 pm</p>
-        </div>
-        <div className="flex flex-row-reverse items-center gap-4 lg:text-xl">
+
+        <div className="flex flex-col pb-4 lg:pb-0 lg:flex-row-reverse w-full lg:justify-start lg:w-[60%]  justify-center items-center gap-4 lg:text-xl relative lg:static ">
           <Image
             src="/images/duck_sleepy.png"
             alt="Sleepy Duck"
-            height={100}
-            width={100}
+            height={120}
+            width={120}
+            className="-left-10 -bottom-6 absolute scale-x-[-1] lg:block lg:scale-x-[1] lg:left-0 lg:bottom-0 lg:static"
           />
-          <p className="relative before:content-[''] before:block before:h-52 before:w-52 before:bg-circle-scribble before:bg-contain before:bg-no-repeat before:absolute before:-top-10 before:-left-8 before:text-mutedOrange font-semibold ">
+          <p className="relative before:content-[''] before:block before:h-52 before:w-52 before:bg-circle-scribble before:bg-contain before:bg-no-repeat before:absolute before:-top-10 before:-left-12 lg:before:-left-8 before:text-mutedOrange font-semibold ">
             Closed Mondays
           </p>
         </div>
-        <div className="flex lg:flex-row flex-col gap-8 z-10 w-[95%] lg:w-full items-center justify-center">
+        <div className="flex lg:flex-row flex-col gap-8 z-10 w-[95%] lg:w-full items-center justify-center pb-2">
           <button
-            className="btn btn-richNavy btn-richNavy:hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-duckyBlue w-full lg:w-1/3"
+            className="btn btn-richNavy btn-richNavy:hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-duckyBlue w-full lg:w-1/3 "
             aria-label="Book A Table"
           >
             <Link href="https://www.tbdine.com/book/restaurant/stone-ducky?idApp=71672&language=en-us">
@@ -59,7 +63,7 @@ export default function Visit() {
             </Link>
           </button>
           <button
-            className="btn btn-mutedOrange btn-mutedOrange:hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-duckyBlue w-full lg:w-1/3"
+            className="btn btn-mutedOrange btn-mutedOrange:hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-duckyBlue w-full lg:w-1/3 "
             aria-label="Order Online"
           >
             <Link
