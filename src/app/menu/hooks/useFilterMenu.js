da7dd2b2f2a89cf61249c46a4eb502bd5d4d288c
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2025-01-22 12:48:41 -0500

    removed a console.log

--- src/app/menu/hooks/useFilterMenu.js
+++ src/app/menu/hooks/useFilterMenu.js
@@ -1,25 +1,29 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
 export const useFilterMenu = (menuData) => {
-     const [isGlutenFree, setIsGlutenFree] = useState(false);
-     const [isDairyFree, setIsDairyFree] = useState(false);
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isDairyFree, setIsDairyFree] = useState(false);
 
-     // filter method that will return menu items that are gluten Free and/or dairy Free
-     const filteredMenu = Object.entries(menuData).reduce(
-       (filtered, [category, items]) => {
-         filtered[category] = items.filter((item) => {
-            console.log(item, 'item');
-           return (
-             (!isGlutenFree || item.glutenFree) &&
-             (!isDairyFree || item.dairyFree)
-           );
-         });
-         return filtered;
-       },
-       {}
-     );
+  // filter method that will return menu items that are gluten Free and/or dairy Free
+  const filteredMenu = Object.entries(menuData).reduce(
+    (filtered, [category, items]) => {
+      filtered[category] = items.filter((item) => {
+        return (
+          (!isGlutenFree || item.glutenFree) && (!isDairyFree || item.dairyFree)
+        );
+      });
+      return filtered;
+    },
+    {}
+  );
 
-     return { isGlutenFree, setIsGlutenFree, isDairyFree, setIsDairyFree, filteredMenu };
-}
+  return {
+    isGlutenFree,
+    setIsGlutenFree,
+    isDairyFree,
+    setIsDairyFree,
+    filteredMenu,
+  };
+};
