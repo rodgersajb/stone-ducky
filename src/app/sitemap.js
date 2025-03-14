2097e893c98a5de09e47b30c6bacffc1d4294743
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2025-03-14 13:00:09 -0400

    restructured sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,34 +1,53 @@
 import { getAllPosts } from "../../lib/api";
-
 export default async function sitemap() {
-  //   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-  const baseUrl = "https://stoneducky.ca";
-  // Static pages
-  const navLinks = [
-    { label: "Menu", href: "/menu" },
-    { label: "Group Bookings", href: "/groups" },
-    { label: "Catering", href: "/catering" },
-    { label: "Contact", href: "/contact" },
-    { label: "Blog", href: "/posts" },
-  ];
-
-  const currentDate = new Date();
-
-  const staticPages = navLinks.map((link) => ({
-    url: `${baseUrl}${link.href}`,
-    lastModified: currentDate,
-    changeFrequency: "weekly",
-    priority: 0.8,
-  }));
-
-  // Dynamic blog posts
   const posts = await getAllPosts();
-  const blogPages = posts.map((post) => ({
-    url: `${baseUrl}/posts/${post.slug}`,
-    lastModified: currentDate,
-    changeFrequency: "monthly",
-    priority: 0.6,
-  }));
+  ;
 
-  return [...staticPages, ...blogPages];
+  const postEntries = posts.map(({ slug }) => {
+    return {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/posts/${slug}`,
+      //   lastModified: new Date(),
+      //   changeFrequency: "monthly",
+      priority: 0.8,
+    };
+  });
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/menu`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/groups`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/catering`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://stoneducky.ca/posts",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    ...postEntries,
+  ];
 }
